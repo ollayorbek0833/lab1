3ac0db2935f2932eaeadaa2cfd1ed51a0a0fd960
Lenovo <ollayorbek2005@gmail.com>
2024-03-03 13:58:36 +0500

    commit

--- problems/problem2.cpp
+++ problems/problem2.cpp
@@ -12,8 +12,8 @@ public:
         cin>>minutes;
         cin>> seconds;
         this->seconds = (seconds>=0&&seconds<=59)?seconds:seconds%60;
-        this->minutes = (minutes>=0&&minutes<=59)?minutes:minutes%60+seconds/60;
-        this->hours = (hours>=0 && hours<=23)?hours:hours%24+minutes/60;
+        this->minutes = (minutes>=0&&minutes<=59)?minutes+seconds/60:minutes%60+seconds/60;
+        this->hours = (hours>=0 && hours<=23)?hours+seconds/60:hours%24+minutes/60;
     }
     void printTime(){
         cout<<hours<<":"<<minutes<<":"<<seconds<<endl;
