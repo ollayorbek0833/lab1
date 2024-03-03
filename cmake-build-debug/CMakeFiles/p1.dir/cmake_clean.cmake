26a733589a226685b470f89b5bdf53f82b6226c9
Lenovo <ollayorbek2005@gmail.com>
2024-03-03 13:56:43 +0500

    commit

---
+++ cmake-build-debug/CMakeFiles/p1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p1.dir/problems/p1.cpp.obj"
+  "CMakeFiles/p1.dir/problems/p1.cpp.obj.d"
+  "libp1.dll.a"
+  "p1.exe"
+  "p1.exe.manifest"
+  "p1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- problems/problem2.cpp
+++ problems/problem2.cpp
@@ -11,9 +11,9 @@ public:
         cin>>hours,
         cin>>minutes;
         cin>> seconds;
-        this->hours = (hours>=0 && hours<=23)?hours:hours%24;
-        this->minutes = (minutes>=0&&minutes<=59)?minutes:minutes%60;
         this->seconds = (seconds>=0&&seconds<=59)?seconds:seconds%60;
+        this->minutes = (minutes>=0&&minutes<=59)?minutes:minutes%60+seconds/60;
+        this->hours = (hours>=0 && hours<=23)?hours:hours%24+minutes/60;
     }
     void printTime(){
         cout<<hours<<":"<<minutes<<":"<<seconds<<endl;
