c61ac80e3c8fb4e1bf2af2958441d5a8b2e45e4f
Lenovo <ollayorbek2005@gmail.com>
2024-03-03 11:18:52 +0500

    problem 2 solved

---
+++ cmake-build-debug/CMakeFiles/p3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p3.dir/problems/p3.cpp.obj"
+  "CMakeFiles/p3.dir/problems/p3.cpp.obj.d"
+  "libp3.dll.a"
+  "p3.exe"
+  "p3.exe.manifest"
+  "p3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
