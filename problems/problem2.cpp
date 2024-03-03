9d81856dc8c8237c51235d59808331a27403da5d
Lenovo <ollayorbek2005@gmail.com>
2024-03-03 12:02:19 +0500

    problem 2 solved

--- problems/problem2.cpp
+++ problems/problem2.cpp
@@ -8,14 +8,14 @@ public:
     int minutes;
     int seconds;
     Timer(int hours, int minutes, int seconds){
-        this->hours = (hours>=0 && hours<=23)?hours:hours-24;
-        this->minutes = (minutes>=0&&minutes<=59)?minutes:minutes-60;
-        this->seconds = (seconds>=0&&seconds<=59)?seconds:seconds-60;
+        cin>>hours,
+        cin>>minutes;
+        cin>> seconds;
+        this->hours = (hours>=0 && hours<=23)?hours:hours%24;
+        this->minutes = (minutes>=0&&minutes<=59)?minutes:minutes&60;
+        this->seconds = (seconds>=0&&seconds<=59)?seconds:seconds&60;
     }
     void printTime(){
-        cin>>hours;
-        cin>>minutes;
-        cin>>seconds;
         cout<<hours<<":"<<minutes<<":"<<seconds<<endl;
     }
 };
