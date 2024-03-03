7e8eb4aa6b0f0ecb6c8663887e285bb142cfde3f
Lenovo <ollayorbek2005@gmail.com>
2024-03-03 12:34:44 +0500

    problem 2 solved

--- problems/problem3.cpp
+++ problems/problem3.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <algorithm>
 
 using namespace std;
 
@@ -22,36 +21,28 @@ public:
 void printGroupCounts(const vector<Student>& students) {
     map<string, int> groupCounts;
 
-    // Count the number of students in each group
     for (const auto& student : students) {
         groupCounts[student.getGroupName()]++;
     }
 
-    // Print the counts in alphabetical order of group names
     for (const auto& pair : groupCounts) {
-        cout << "Group " << pair.first << ": " << pair.second << " students" << endl;
+        cout <<pair.first << ": " << pair.second << endl;
     }
 }
 
 int main() {
     int n;
-    cout << "Enter the number of students: ";
+    cout << "Input: ";
     cin >> n;
 
     vector<Student> students;
     string name, group;
     long int id;
 
-    cout << "Enter student details (name id group):" << endl;
     for (int i = 0; i < n; ++i) {
         cin >> name >> id >> group;
         students.emplace_back(name, id, group);
     }
-
-    // Sort students based on group name
-    sort(students.begin(), students.end(), [](const Student& s1, const Student& s2) {
-        return s1.getGroupName() < s2.getGroupName();
-    });
 
     printGroupCounts(students);
 
